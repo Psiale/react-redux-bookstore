c6aff712dc59a2dca861b2cefac2b24881698079
Rory Hellier <rory.hellier@googlemail.com>
2021-01-25 21:06:17 +0000

    Amended the App.js component to be an ES6 arrow function

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
+import React from 'react';
 import BooksForm from './BooksForm';
 import BooksList from '../containers/BooksList';
 
-function App() {
-  return (
-    <div className="App">
-      <BooksList />
-      <BooksForm />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <BooksList />
+    <BooksForm />
+  </div>
+);
 
 export default App;
